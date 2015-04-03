dc39ac01092c242d9b6798ce0ffe7e135da2d40b
Ilya Radchenko <ilya@burstcreations.com>
2015-04-03 15:49:23 -0400

    Add bower imports, js and less

--- files/Brocfile.js
+++ files/Brocfile.js
@@ -1,6 +1,9 @@
 /* global require, module */
 
+var path = require('path');
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
 
 var app = new EmberApp();
 
@@ -17,4 +20,21 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+importAssets(app, app.bowerDirectory, [
+  'bootstrap/dist/js/bootstrap.js'
+]);
+
+// Copy fontawesome fonts
+var fonts = pickFiles(app.bowerDirectory + '/fontawesome/fonts', {
+  srcDir: '/',
+  files: ['**/*'],
+  destDir: '/assets/fonts'
+});
+
+module.exports = mergeTrees([app.toTree(), fonts]);
+
+function importAssets(app, baseUrl, urls) {
+  urls.forEach(function (url) {
+    app.import(path.join(baseUrl, url));
+  });
+}
